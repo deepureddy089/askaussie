5b386b7555240b85e0f6309a1b7a778e1a6eb1e7
deepureddy089 <deepureddy089@gmail.com>
2025-07-16 00:48:16 +0930

    mid night edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
-       
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
 
       <body className={`${font.className} antialiased`}>{children}</body>
